67a46c9513dc7d7c5ee2f75e29c1d76930a21958
KiKi_IamTheStrongestOfThem <ratneshwarkumarsingh0@gmail.com>
2025-10-01 18:54:51 +0530

    Create important.cs

---
+++ important.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("This is the way you are directed to use");
+        Console.WriteLine("If you don't have Lua setup read extra.md");
+        Console.WriteLine("To enjoy this project:-");
+        Console.WriteLine("1.) Download or copy the Repository");
+        Console.WriteLine("2.) Run = Cargo run");
+        Console.WriteLine("3.) Enjoy just close the terminal to stop this.");
+    }
+}
