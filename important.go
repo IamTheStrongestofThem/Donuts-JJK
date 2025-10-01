2aa20c5f6768a5316dca980e6b01fd0a7103b3fb
KiKi_IamTheStrongestOfThem <ratneshwarkumarsingh0@gmail.com>
2025-10-01 18:17:25 +0530

    The way you get it

---
+++ important.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("This is the way you are directed to use")
+  fmt.Println("If you don't have Go installed read extra.md)
+  fmt.Println("To enjoy this project:-")
+  fmt.Println("1.) Downlaod or copy the Respiratory")
+  fmt.Println("2.) Run = Cargo run")
+  fmt.Println("3.) Enjoy just close the terminal to stop this.)
+}
