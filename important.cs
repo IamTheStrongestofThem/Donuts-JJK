2cdb6897eef735178b83ad1ecf521a7ce0b321c9
KiKi_IamTheStrongestOfThem <ratneshwarkumarsingh0@gmail.com>
2025-10-01 18:57:12 +0530

    Update important.cs

--- important.cs
+++ important.cs
@@ -3,7 +3,7 @@ using System;
 class Program {
     static void Main() {
         Console.WriteLine("This is the way you are directed to use");
-        Console.WriteLine("If you don't have Lua setup read extra.md");
+        Console.WriteLine("If you don't have .net SDK setup read extra.md");
         Console.WriteLine("To enjoy this project:-");
         Console.WriteLine("1.) Download or copy the Repository");
         Console.WriteLine("2.) Run = Cargo run");
