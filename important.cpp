e4e5e715c197725dcb4677df24415e67d9ed8957
KiKi_IamTheStrongestOfThem <ratneshwarkumarsingh0@gmail.com>
2025-10-01 18:55:27 +0530

    Create important.cpp

---
+++ important.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is the way you are directed to use" << endl;
+    cout << "If you don't have Lua setup read extra.md" << endl;
+    cout << "To enjoy this project:-" << endl;
+    cout << "1.) Download or copy the Repository" << endl;
+    cout << "2.) Run = Cargo run" << endl;
+    cout << "3.) Enjoy just close the terminal to stop this." << endl;
+    return 0;
+}
